f7f692ec8d33f5aeb2d912d43007026ea21a178d
shanedabes <shane@isda.best>
2020-11-15 00:16:39 +0000

    add argument for http port

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ type Options struct {
 	UseTLS   bool     `short:"T" long:"tls" env:"GOWON_TLS" description:"Connect to irc server using tls"`
 	Verbose  bool     `short:"v" long:"verbose" env:"GOWON_VERBOSE" description:"Verbose logging"`
 	Debug    bool     `short:"d" long:"debug" env:"GOWON_DEBUG" description:"Debug logging"`
+	HTTPPort string   `short:"p" long:"http-port" env:"GOWON_HTTP_PORT" default:"1337" description:"http listen port"`
 }
 
 func splitOptArray(sa []string) []string {
@@ -92,7 +93,7 @@ func main() {
 
 	h := httpHandler{irccon}
 
-	l, err := net.Listen("tcp", ":1337")
+	l, err := net.Listen("tcp", fmt.Sprintf(":%s", opts.HTTPPort))
 	if err != nil {
 		log.Fatal(err)
 	}
