ca60b3ce1e5ec163958c9d1374d34b3bb80850a2
shanedabes <shane@isda.best>
2021-05-14 02:32:11 +0100

    use env-delim to split channels environment variable

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"strings"
 
 	"github.com/jessevdk/go-flags"
 	irc "github.com/thoj/go-ircevent"
@@ -12,22 +11,12 @@ type Options struct {
 	Server   string   `short:"s" long:"server" env:"GOWON_SERVER" required:"true" description:"IRC server:port"`
 	Nick     string   `short:"n" long:"nick" env:"GOWON_NICK" required:"true" description:"Bot nick"`
 	User     string   `short:"u" long:"user" env:"GOWON_USER" required:"true" description:"Bot user"`
-	Channels []string `short:"c" long:"channels" env:"GOWON_CHANNELS" required:"true" description:"Channels to join"`
+	Channels []string `short:"c" long:"channels" env:"GOWON_CHANNELS" env-delim:"," required:"true" description:"Channels to join"`
 	UseTLS   bool     `short:"T" long:"tls" env:"GOWON_TLS" description:"Connect to irc server using tls"`
 	Verbose  bool     `short:"v" long:"verbose" env:"GOWON_VERBOSE" description:"Verbose logging"`
 	Debug    bool     `short:"d" long:"debug" env:"GOWON_DEBUG" description:"Debug logging"`
 	Prefix   string   `short:"P" long:"prefix" env:"GOWON_PREFIX" default:"." description:"prefix for commands"`
 	Broker   string   `short:"b" long:"broker" env:"GOWON_BROKER" default:"localhost:1883" description:"mqtt broker"`
-}
-
-func splitOptArray(sa []string) []string {
-	out := []string{}
-
-	for _, s := range sa {
-		out = append(out, strings.Split(s, ",")...)
-	}
-
-	return out
 }
 
 func supCommand() string {
@@ -46,8 +35,6 @@ func main() {
 		log.Fatal(err)
 	}
 
-	channels := splitOptArray(opts.Channels)
-
 	irccon := irc.IRC(opts.Nick, opts.User)
 
 	irccon.VerboseCallbackHandler = opts.Verbose
@@ -55,7 +42,7 @@ func main() {
 	irccon.UseTLS = opts.UseTLS
 
 	irccon.AddCallback("001", func(e *irc.Event) {
-		for _, channel := range channels {
+		for _, channel := range opts.Channels {
 			irccon.Join(channel)
 		}
 	})
