69bf6db9553ce3f997cb9e69a2755f6778401901
shanedabes <shane@isda.best>
2021-05-18 00:26:13 +0100

    add mqtt reconnect to gowon and test module 1

--- demo/module1/main.go
+++ demo/module1/main.go
@@ -7,6 +7,7 @@ import (
 	"os/signal"
 	"strings"
 	"syscall"
+	"time"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"github.com/gowon-irc/gowon/pkg/message"
@@ -17,6 +18,8 @@ type Options struct {
 	Prefix string `short:"P" long:"prefix" env:"GOWON_PREFIX" default:"." description:"prefix for commands"`
 	Broker string `short:"b" long:"broker" env:"GOWON_BROKER" default:"localhost:1883" description:"mqtt broker"`
 }
+
+const mqttConnectRetryInternal = 5 * time.Second
 
 func capHandler(m message.Message) string {
 	return strings.ToUpper(m.GetArgs())
@@ -32,6 +35,8 @@ func main() {
 
 	mqttOpts := mqtt.NewClientOptions().AddBroker(fmt.Sprintf("tcp://%s", opts.Broker))
 	mqttOpts.SetClientID("gowon_module1")
+	mqttOpts.SetConnectRetry(true)
+	mqttOpts.SetConnectRetryInterval(mqttConnectRetryInternal)
 
 	c := mqtt.NewClient(mqttOpts)
 	if token := c.Connect(); token.Wait() && token.Error() != nil {
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"github.com/gowon-irc/gowon/pkg/message"
@@ -21,6 +22,8 @@ type Options struct {
 	Prefix   string   `short:"P" long:"prefix" env:"GOWON_PREFIX" default:"." description:"prefix for commands"`
 	Broker   string   `short:"b" long:"broker" env:"GOWON_BROKER" default:"localhost:1883" description:"mqtt broker"`
 }
+
+const mqttConnectRetryInternal = 5 * time.Second
 
 func createMessageHandler(irccon *irc.Connection) mqtt.MessageHandler {
 	return func(client mqtt.Client, msg mqtt.Message) {
@@ -45,6 +48,8 @@ func main() {
 
 	mqttOpts := mqtt.NewClientOptions().AddBroker(fmt.Sprintf("tcp://%s", opts.Broker))
 	mqttOpts.SetClientID("gowon")
+	mqttOpts.SetConnectRetry(true)
+	mqttOpts.SetConnectRetryInterval(mqttConnectRetryInternal)
 
 	c := mqtt.NewClient(mqttOpts)
 	if token := c.Connect(); token.Wait() && token.Error() != nil {
