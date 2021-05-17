20ea253eb8076257c8a79dff8cbefe60f0c4899e
shanedabes <shane@isda.best>
2021-05-17 12:36:52 +0100

    add example module

---
+++ demo/module1/go.mod
@@ -0,0 +1,9 @@
+module github.com/gowon-irc/gowon/demo/module1
+
+go 1.16
+
+require (
+	github.com/eclipse/paho.mqtt.golang v1.3.4
+	github.com/gowon-irc/gowon v0.0.0-20210517020741-c6103d4b1b7c
+	github.com/jessevdk/go-flags v1.5.0
+)
---
+++ demo/module1/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/gowon-irc/gowon/pkg/message"
+	"github.com/jessevdk/go-flags"
+)
+
+type Options struct {
+	Prefix string `short:"P" long:"prefix" env:"GOWON_PREFIX" default:"." description:"prefix for commands"`
+	Broker string `short:"b" long:"broker" env:"GOWON_BROKER" default:"localhost:1883" description:"mqtt broker"`
+}
+
+func capHandler(m message.Message) string {
+	return strings.ToUpper(m.GetArgs())
+}
+
+func main() {
+	opts := Options{}
+
+	_, err := flags.Parse(&opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mqttOpts := mqtt.NewClientOptions().AddBroker(fmt.Sprintf("tcp://%s", opts.Broker))
+	mqttOpts.SetClientID("gowon_module1")
+
+	c := mqtt.NewClient(mqttOpts)
+	if token := c.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	c.Subscribe("/gowon/input", 0, func(client mqtt.Client, msg mqtt.Message) {
+		ms, err := message.CreateMessageStruct(msg.Payload())
+		if err != nil {
+			log.Print(err)
+
+			return
+		}
+
+		var out string
+
+		switch ms.GetCommand() {
+		case "cap":
+			out = capHandler(ms)
+		default:
+			return
+		}
+
+		mb, _ := message.CreateMessageBody(ms.Dest, out, ms.Nick)
+		client.Publish("/gowon/output", 0, false, mb)
+	})
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	<-sigs
+}
