2b732a7cea775bc5ad0c1fa749b0e3f50c0122df
shanedabes <shane@isda.best>
2021-05-30 02:55:24 +0100

    add filter functions

---
+++ pkg/message/filter.go
@@ -0,0 +1,112 @@
+package message
+
+import (
+	"regexp"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+const ErrorFilterInvalidChars = "filter contains invalid characters"
+
+const ErrorFilterInvertNotAtBeginning = "Inversion (!) can only exist at the beginning of the filter"
+
+const ErrorFilterInvalidFields = "Filter does not contain two fields, equals should appear once"
+
+const ErrorFilterInvalidKey = "Filter key is invalid, must be one of module, msg, nick, dest, command, args"
+
+func checkFilterSingle(filter string) error {
+	m, err := regexp.MatchString(`^[!=a-zA-Z]+$`, filter)
+	if err != nil {
+		return err
+	}
+	if !m {
+		return errors.New(ErrorFilterInvalidChars)
+	}
+
+	m, err = regexp.MatchString(`[=a-zA-Z]!`, filter)
+	if err != nil {
+		return err
+	}
+	if m {
+		return errors.New(ErrorFilterInvertNotAtBeginning)
+	}
+
+	if strings.Count(filter, "=") != 1 {
+		return errors.New(ErrorFilterInvalidFields)
+	}
+
+	contains := func(ss []string, c string) bool {
+		for _, s := range ss {
+			if s == c {
+				return true
+			}
+		}
+		return false
+	}
+
+	key := strings.TrimPrefix(strings.Split(filter, "=")[0], "!")
+	if !contains([]string{"module", "msg", "nick", "dest", "command", "args"}, key) {
+		return errors.New(ErrorFilterInvalidKey)
+	}
+
+	return nil
+}
+
+func CheckFilter(filter string) error {
+	for _, f := range strings.Split(filter, "+") {
+		err := checkFilterSingle(f)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func invert(b, inv bool) bool {
+	if inv {
+		return !b
+	}
+
+	return b
+}
+
+func filterSingle(m *Message, filter string) (bool, error) {
+	err := checkFilterSingle(filter)
+	if err != nil {
+		return false, err
+	}
+
+	out := true
+
+	messageFields := map[string]string{
+		"module": m.Module,
+	}
+
+	invertFilter := strings.HasPrefix(filter, "!")
+	kv := strings.Split(strings.TrimPrefix(filter, "!"), "=")
+	key, value := kv[0], kv[1]
+
+	if messageFields[key] == value {
+		out = true
+	}
+
+	return invert(out, invertFilter), nil
+}
+
+func Filter(m *Message, filter string) (bool, error) {
+	for _, f := range strings.Split(filter, "+") {
+		res, err := filterSingle(m, f)
+
+		if err != nil {
+			return false, err
+		}
+
+		if !res {
+			return false, nil
+		}
+	}
+
+	return true, nil
+}
---
+++ pkg/message/filter_test.go
@@ -0,0 +1,222 @@
+package message
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCheckFilterSingleErrors(t *testing.T) {
+	cases := []struct {
+		name   string
+		filter string
+		errMsg string
+	}{
+		{
+			name:   "Invalid characters",
+			filter: "module=m/",
+			errMsg: ErrorFilterInvalidChars,
+		},
+		{
+			name:   "Inversion in middle",
+			filter: "mmodule!=m",
+			errMsg: ErrorFilterInvertNotAtBeginning,
+		},
+		{
+			name:   "Multiple fields",
+			filter: "module=m=m",
+			errMsg: ErrorFilterInvalidFields,
+		},
+		{
+			name:   "No fields",
+			filter: "module",
+			errMsg: ErrorFilterInvalidFields,
+		},
+		{
+			name:   "Invalid key",
+			filter: "m=m",
+			errMsg: ErrorFilterInvalidKey,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := checkFilterSingle(tc.filter)
+			assert.EqualError(t, err, tc.errMsg)
+		})
+	}
+}
+
+func TestCheckFilterErrors(t *testing.T) {
+	cases := []struct {
+		name   string
+		filter string
+		resNil bool
+	}{
+		{
+			name:   "Multiple valid filters",
+			filter: "module=m+!dest=d",
+			resNil: true,
+		},
+		{
+			name:   "One valid filter",
+			filter: "module=m",
+			resNil: true,
+		},
+		{
+			name:   "One invalid filter",
+			filter: "module!=m",
+			resNil: false,
+		},
+		{
+			name:   "One invalid filter, one correct",
+			filter: "module=m/+dest=d",
+			resNil: false,
+		},
+		{
+			name:   "Multiple invalid filters",
+			filter: "module=m/+dest!=d",
+			resNil: false,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := CheckFilter(tc.filter)
+			assert.Equal(t, tc.resNil, got == nil)
+		})
+	}
+}
+
+func TestFilterSingleReturnsError(t *testing.T) {
+	_, err := filterSingle(&Message{}, "module!=m")
+	assert.NotNil(t, err)
+}
+
+var filters = []struct {
+	name    string
+	message Message
+	filter  string
+	result  bool
+}{
+	{
+		name: "Filter module",
+		message: Message{
+			Module: "mod",
+		},
+		filter: "module=mod",
+		result: true,
+	},
+	{
+		name: "Filter module with inversion",
+		message: Message{
+			Module: "mod",
+		},
+		filter: "!module=mod",
+		result: false,
+	},
+	{
+		name: "Filter command",
+		message: Message{
+			Command: "test",
+		},
+		filter: "command=test",
+		result: true,
+	},
+}
+
+func TestCheckFilterSingleValid(t *testing.T) {
+	for _, tc := range filters {
+		t.Run(tc.name, func(t *testing.T) {
+			err := checkFilterSingle(tc.filter)
+			assert.Nil(t, err)
+		})
+	}
+}
+
+func TestFilterMessageSingle(t *testing.T) {
+	for _, tc := range filters {
+		t.Run(tc.name, func(t *testing.T) {
+			got, _ := filterSingle(&tc.message, tc.filter)
+
+			assert.Equal(t, got, tc.result)
+		})
+	}
+}
+
+func TestInvert(t *testing.T) {
+	cases := []struct {
+		name     string
+		b        bool
+		inv      bool
+		expected bool
+	}{
+		{
+			name:     "Invert true",
+			b:        true,
+			inv:      true,
+			expected: false,
+		},
+		{
+			name:     "Don't invert true",
+			b:        true,
+			inv:      false,
+			expected: true,
+		},
+		{
+			name:     "Invert false",
+			b:        false,
+			inv:      true,
+			expected: true,
+		},
+		{
+			name:     "Don't invert false",
+			b:        false,
+			inv:      false,
+			expected: false,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := invert(tc.b, tc.inv)
+			assert.Equal(t, tc.expected, got)
+		})
+	}
+}
+
+func TestFilter(t *testing.T) {
+	cases := []struct {
+		name    string
+		message Message
+		filter  string
+		result  bool
+	}{
+		{
+			name: "One true",
+			message: Message{
+				Module: "mod",
+			},
+			filter: "module=mod",
+			result: true,
+		},
+		{
+			name: "Multiple true",
+			message: Message{
+				Module:  "mod",
+				Command: "test",
+			},
+			filter: "module=mod+command=test",
+			result: true,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := Filter(&tc.message, tc.filter)
+
+			assert.Nil(t, err)
+			assert.Equal(t, got, tc.result)
+		})
+	}
+}
